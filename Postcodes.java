acafd5a4ec23a491144709dde5326b415af56c84
Adam Kogut <adamkgt@gmail.com>
2020-12-14 14:55:59 +0100

    #2 Przechowywanie kodów pocztowych oraz ich wyświetlanie

---
+++ Postcodes.java
@@ -0,0 +1,21 @@
+class App{
+	public static void main(String args[]){
+		int[] postcodes = new int[4];
+		
+		postcodes[0]= 72603;
+		postcodes[1]= 72600;
+		postcodes[2]= 72602;
+		postcodes[3]= 72605;
+
+		System.out.println("---------------------");
+		
+		System.out.println(postcodes[0]);
+		System.out.println(postcodes[1]);
+		System.out.println(postcodes[2]);		
+		System.out.println(postcodes[3]);	
+		System.out.println("itd.");	
+		
+		System.out.println("---------------------");
+		
+		}
+}
